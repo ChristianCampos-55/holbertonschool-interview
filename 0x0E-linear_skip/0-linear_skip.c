7facaf458134167bdde53786f86701ce86eda459
ChristianCampos-55 <gamochris@gmail.com>
2021-11-04 19:02:58 -0500

    [ADD]Tested project + folder + Readme

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,53 @@
+#include "search.h"
+
+/**
+ * linear_skip - Looks for a value in a list
+ * @list: Pointer to the head of said list
+ * @value: Value to to be searched
+ *
+ * Return: Pointer to value searched or NULL upon failure
+**/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *skipper, *prev;
+	skipper = list->f;
+	prev = list;
+
+	while (skipper)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+			   skipper->index, skipper->n);
+
+		if (skipper->n >= value || !skipper->f)
+		{
+
+			if (!skipper->f && skipper->n < value)
+			{
+				prev = skipper;
+				while (skipper->next)
+					skipper = skipper->next;
+			}
+
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				   prev->index, skipper->index);
+
+			while (prev)
+			{
+				printf("Value checked at index [%lu] = [%d]\n",
+					   prev->index, prev->n);
+				if (prev->n == value)
+					return (prev);
+				if (prev->n > value)
+					return (NULL);
+				prev = prev->next;
+			}
+
+			break;
+
+		}
+		prev = skipper;
+		skipper = skipper->f;
+	}
+	return (NULL);
+}
