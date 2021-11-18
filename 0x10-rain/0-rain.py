fe72703a641bbff9f1aad1d9b0cd2826940ea138
ChristianCampos-55 <gamochris@gmail.com>
2021-11-18 10:33:00 -0500

    [UPDT]Updated code

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,12 +6,18 @@ Calculates how much water will be retained after it rains
 
 def rain(walls):
     """
-    Rain function
+    Given a list of non-negative integers representing walls of width 1,
+    calculate how much water will be retained after it rains.
     """
-    if type(walls) is not list:
-        return 0
     if len(walls) is 0:
         return 0
     if (len(walls) < 2):
         return 0
-    return sum([i for i in walls[1:-1] if i > 0])
+    counter = 0
+    for flatter, medder in enumerate(walls):
+        if flatter is not 0 and flatter is not (len(walls) - 1):
+            left = max(walls[0:flatter])
+            right = max(walls[flatter + 1:len(walls)])
+            if medder < left and medder < right:
+                counter += min([left, right]) - medder
+    return counter
