97da70d3ba21fc19c936bc454d9e76d8e24a4ad3
ChristianCampos-55 <gamochris@gmail.com>
2021-11-25 22:31:30 -0500

    [REF]Changed whole code, made simpler

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,93 +1,71 @@
-#include "sort.h"
+#include "heap_sorter.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * heap_sort - Sorts an array in ascending order.
+ * heap_sort - Function that sorts an array in ascending order
  * @array: array to sort
- * @size: array type
+ * @size: array's size
+ * Return: zilch
  */
 
 void heap_sort(int *array, size_t size)
 {
-	int a;
-	size_t h_s = size;
+	int i, holder;
 
-	heap_maxer(array, size);
-
-	for (a = size - 1; a >= 1; a--)
+	if (size < 2)
 	{
-		flip(&array[0], &array[a]);
-		h_s--;
-		sift_down(array, 0, h_s);
-	}
-}
-
-/**
- * heap_maxer - Converts a heap to a list.
- * @array: array to srot
- * @size: array type
- */
-
-void heap_maxer(int *array, size_t size)
-{
-	int a;
-
-	for (a = size / 2; a >= 0; a--)
-		sift_down(array, a, size);
-}
-
-/**
- * sift_down - Moves an element to its correct index on the list.
- * @array: array of integers
- * @a: long array
- * @h_s: integer
- */
-
-void sift_down(int *array, size_t a, size_t h_s)
-{
-	size_t c = 2 * a + 1;
-	size_t n = sizeof(array) + 2
-
-	if (c >= h_s)
 		return;
+	}
 
-	if ((c + 1 < h_s) && swapper(array[c + 1], array[c]) > 0)
-		c++;
-
-	if (swapper(array[c], array[a]) > 0)
+	for (i = size / 2 - 1; i >= 0; i--)
 	{
-		flip(&array[c], &array[a]);
-		print_array(array, n);
-		sift_down(array, c, h_s);
+		swapper(array, size, i, size);
+	}
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		holder = array[0];
+		array[0] = array[i];
+		array[i] = holder;
+		if (i != 0)
+			print_array(array, size);
+		swapper(array, i, 0, size);
 	}
 }
 
 /**
- * flip - Changes the first element of the array with the last.
- * Decrease the considered range of the list by one.
- * @a: int to change
- * @b: int to change
+ * swapper - Function to swap values
+ * @array: array to swap
+ * @size: array's size
+ * @i: starter
+ * @s: size int
+ * Return: Zilch
  */
-void flip(int *a, int *b)
+
+void swapper(int *array, int size, int i, int s)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	int bgest = i, holder;
+	int a = 2 * i + 1;
+	int b = 2 * i + 2;
+
+	if (a < size && array[a] > array[bgest])
+	{
+		bgest = a;
+	}
+
+	if (b < size && array[b] > array[bgest])
+	{
+		bgest = b;
+	}
+
+	if (bgest != i)
+	{
+		holder = array[i];
+		array[i] = array[bgest];
+		array[bgest] = holder;
+		print_array(array, s);
+		swapper(array, size, bgest, s);
+	}
 }
 
-/**
- * swapper - check if a is larger than b, and if so swaps them.
- * @a: int to change
- * @b: int to change
- * Return: Swapped int
- */
-int swapper(int a, int b)
-{
-	if (a > b)
-		return (1);
-	else if (a < b)
-		return (-1);
-	else
-		return (0);
-}
---
+++ 0x11-heap_sort/heap_sorter.h
@@ -0,0 +1,10 @@
+#ifndef _HEAP_SORTER_H
+#define _HEAP_SORTER_H
+
+#include <stddef.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void swapper(int *array, int size, int i, int s);
+
+#endif
